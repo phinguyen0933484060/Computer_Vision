404ea65ef1f66c44f6e0712dcd6d42db5c2c8068
phinguyen0933484060 <nguyenlamphiyugi282001@gmail.com>
2023-02-15 05:31:00 -0500

    Hand - Br

--- BT_TUAN1/BT1.cpp
+++ BT_TUAN1/BT1.cpp
@@ -10,29 +10,31 @@ int main()
     // ofstream fnohand("NoHand.txt");
 
     Mat BackGround = imread("NoHand.png");
-    Mat Hand = imread ("Hand.png");
-    resize(Hand,Hand,Size(600,420),INTER_LINEAR);
-    resize(BackGround,BackGround,Size(600,420),INTER_LINEAR);
-    Mat diffImage,final;
-    absdiff(Hand,BackGround,diffImage);
-    Mat Mask = Mat::zeros(diffImage.rows,diffImage.cols,CV_8UC1);
+    Mat Hand = imread("Hand.png");
+    resize(Hand, Hand, Size(600, 420), INTER_LINEAR);
+    resize(BackGround, BackGround, Size(600, 420), INTER_LINEAR);
+    Mat diffImage, final;
+    absdiff(Hand, BackGround, diffImage);
+    //medianBlur(diffImage,diffImage,5);
+    //GaussianBlur(diffImage, diffImage, Size(3, 3), 0);
+    Mat Mask = Mat::zeros(diffImage.rows, diffImage.cols, CV_8UC1);
     float threshold_0 = 30.0f;
-    
-    for(int i = 0 ; i < diffImage.rows; ++i)
-        for (int j = 0 ; j < diffImage.cols ; ++j )
-        {
-            Vec3b pix = diffImage.at<Vec3b>(i,j);
-            if(sqrt(pix[0]*pix[0]+pix[1]*pix[1]+pix[2]*pix[2]) > threshold_0)
-                Mask.at<unsigned char>(i,j) = 255;
 
+    for (int i = 0; i < diffImage.rows; ++i)
+        for (int j = 0; j < diffImage.cols; ++j)
+        {
+            Vec3b pix = diffImage.at<Vec3b>(i, j);
+            if (sqrt(pix[0] * pix[0] + pix[1] * pix[1] + pix[2] * pix[2]) > threshold_0)
+                Mask.at<unsigned char>(i, j) = 255;
         }
     ofstream fo("Diff.txt");
-    fo <<"DIff"<< diffImage<<endl;
-    fo <<"MASK"<<Mask <<endl;
-    cvtColor(diffImage,final ,COLOR_RGB2GRAY);
-    threshold(final,final,0,255,0);
+    // fo <<"DIff"<< diffImage<<endl;
+    // fo <<"MASK"<<Mask <<endl;
+    // cvtColor(diffImage,final ,COLOR_RGB2GRAY);
+    threshold(final, final, 0, 255, 0);
+    GaussianBlur(Mask,Mask,Size(3,3),4);
     // imshow("final",final);
-    imshow("mask",Mask);
+    imshow("mask", Mask);
     waitKey(0);
     // fnohand << "BackGround" <<BackGroundImage<<endl;
     // fhand << "Hand"<<Hand<<endl;
